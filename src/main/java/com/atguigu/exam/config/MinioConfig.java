5ae29c11f49305015f8eca28c15321b343704cb2
我只有一岁半 <2307585872@qq.com>
2025-11-07 10:37:24 +0800

    feature: 饿汉式在启动时就加载 minio 连接

---
+++ src/main/java/com/atguigu/exam/config/MinioConfig.java
@@ -0,0 +1,27 @@
+package com.atguigu.exam.config;
+
+import com.atguigu.exam.config.properties.MinioProperties;
+import io.minio.MinioClient;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class MinioConfig {
+
+    private final MinioProperties minioProperties;
+
+    @Bean
+    public MinioClient minioClient() {
+        MinioClient minioClient = MinioClient.builder()
+                .endpoint(minioProperties.getEndpoint())
+                .credentials(minioProperties.getAccessKey(), minioProperties.getSecretKey())
+                .build();
+        log.info("MinioClient初始化成功,连接对象信息为:{}" , minioClient);
+        return minioClient;
+    }
+
+}
